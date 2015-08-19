c9c084f7fa8f4f168913caaf9a0fd207119f3b7d
Brian Woodward <brian.woodward@gmail.com>
2015-08-18 20:57:06 -0400

    tests

--- index.js
+++ index.js
@@ -7,6 +7,60 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
+var lazy = require('lazy-cache')(require);
+lazy('util');
+lazy('ansi-yellow');
+lazy('ansi-green');
+lazy('ansi-cyan');
+lazy('ansi-red');
+
+module.exports = function (composer, options) {
+  options = options || {};
+
+  if (typeof options.colors === 'undefined') {
+    options.colors = true;
+  }
+
+  // use the stderr stream by default so other output
+  // can be piped into a file with `> file.txt`
+  var stream = options.stream || process.stderr;
+
+  function write() {
+    stream.write(lazy.util.format.apply(null, arguments));
+  }
+
+  function writeln() {
+    write.apply(null, [].slice.call(arguments).concat(['\n']));
+  }
+
+  // setup some listeners
+  composer.on('task.starting', function (info) {
+    var task = info.task;
+    var run = info.run;
+    if (options.colors) {
+      writeln(lazy.ansiGreen('starting'), lazy.ansiCyan('[' + task.name + ']'), run.start.toTimeString());
+    } else {
+      writeln('starting', '[' + task.name + ']', run.start.toTimeString());
+    }
+  });
+
+  composer.on('task.finished', function (info) {
+    var task = info.task;
+    var run = info.run;
+    if (options.colors) {
+      writeln(lazy.ansiYellow('finished'), lazy.ansiCyan('[' + task.name + ']'), run.end.toTimeString());
+    } else {
+      writeln('finished', '[' + task.name + ']', run.end.toTimeString());
+    }
+  });
+
+  composer.on('task.error', function (err, info) {
+    console.error(info);
+    var task = info.task;
+    if (options.colors) {
+      writeln(lazy.ansiRed('ERROR'), lazy.ansiCyan('[' + task.name + ']'), err);
+    } else {
+      writeln('ERROR', '[' + task.name + ']', err);
+    }
+  });
 };
---
+++ test.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var assert = require('assert');
+var Composer = require('composer').Composer;
+var runtimes = require('./');
+
+var composer;
+
+describe('composer-runtimes', function () {
+  beforeEach(function () {
+    composer = new Composer();
+  });
+
+  it('should add listeners to composer', function (done) {
+    var restore = captureOutput(process.stderr);
+    runtimes(composer);
+    var count = 0;
+    composer.task('test', function (cb) {
+      count++;
+      cb();
+    });
+    composer.run('test', function (err) {
+      var output = restore();
+      if (err) return done(err);
+      assert.equal(count, 1);
+      assert.equal(output.length, 2);
+      assert.notEqual(output[0][0].indexOf('starting'), -1);
+      assert.notEqual(output[1][0].indexOf('finished'), -1);
+      done();
+    });
+  });
+
+  it('should output messages without colors', function (done) {
+    var restore = captureOutput(process.stderr);
+    runtimes(composer, {colors: false});
+    var count = 0;
+    composer.task('test', function (cb) {
+      count++;
+      cb();
+    });
+    composer.run('test', function (err) {
+      var output = restore();
+      if (err) return done(err);
+      assert.equal(count, 1);
+      assert.equal(output.length, 2);
+      assert.notEqual(output[0][0].indexOf('starting'), -1);
+      assert.notEqual(output[1][0].indexOf('finished'), -1);
+      assert.equal(output[0][0].indexOf('\u001b'), -1);
+      assert.equal(output[1][0].indexOf('\u001b'), -1);
+      done();
+    });
+  });
+
+  it('should output messages to another stream', function (done) {
+    var restore = captureOutput(process.stdout);
+    runtimes(composer, {stream: process.stdout});
+    var count = 0;
+    composer.task('test', function (cb) {
+      count++;
+      cb();
+    });
+    composer.run('test', function (err) {
+      var output = restore();
+      if (err) return done(err);
+      assert.equal(count, 1);
+      assert.equal(output.length, 2);
+      assert.notEqual(output[0][0].indexOf('starting'), -1);
+      assert.notEqual(output[1][0].indexOf('finished'), -1);
+      done();
+    });
+  });
+});
+
+function captureOutput (stream) {
+  var output = [];
+  var write = stream.write;
+  stream.write = function () {
+    output.push([].slice.call(arguments));
+  };
+  return function restore () {
+    stream.write = write;
+    return output;
+  };
+}
