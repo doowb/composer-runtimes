42ffe761e5480ab9be6769360c17201bbc6a670f
Brian Woodward <brian.woodward@gmail.com>
2015-08-18 20:17:14 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * composer-runtimes <https://github.com/doowb/composer-runtimes>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
